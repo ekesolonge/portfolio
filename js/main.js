1b8edcb69a234c6cb22065d9a4845d1f76405e7b
Ekeso <ekesolonge@gmail.com>
2020-08-28 21:45:17 +0100

    Added Loader

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
-const hamburger = document.querySelector('.hamburger-box');
-const nav = document.querySelector('.nav-list');
+// Hamburger Menu
+const hamburger = document.querySelector(".hamburger-box");
+const nav = document.querySelector(".nav-list");
 let menuOpen = false;
-hamburger.addEventListener('click', () => {
-  if(!menuOpen) {
-    hamburger.classList.add('open');
-    nav.classList.add('show');
+hamburger.addEventListener("click", () => {
+  if (!menuOpen) {
+    hamburger.classList.add("open");
+    nav.classList.add("show");
     menuOpen = true;
   } else {
-    hamburger.classList.remove('open');
-    nav.classList.remove('show');
+    hamburger.classList.remove("open");
+    nav.classList.remove("show");
     menuOpen = false;
+  }
+});
+
+// Loader
+window.addEventListener("load", function (e) {
+  const loader = document.querySelector(".loader-area");
+  const landing = document.querySelector(".landing");
+  if (e.target.readyState === "complete") {
+    setTimeout(function () {
+      loader.style.opacity = "0";
+      loader.style.display = "none";
+      setTimeout(function () {
+        landing.style.opacity = "1";
+      }, 0);
+    }, 3000);
   }
 });
